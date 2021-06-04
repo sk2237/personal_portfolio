ba2022737c6def60d31bc89f77fb28e2b7ea3fb8
sk2237 <suman77327@gmail.com>
2021-06-04 02:38:03 +0000

    remove old files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+-2_=z4h8br^a80o&1_3aylw*7r%kk^nf+7gpj2b%8h=ohdm$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sumankr2237.pythonanywhere.com']
 
 
 # Application definition
